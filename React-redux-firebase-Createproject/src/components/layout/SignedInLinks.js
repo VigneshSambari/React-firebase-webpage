fdfd07b69ef2dffef0ffcd1696cb237ddaf86a1c
VickySam1901 <72963230+VickySam1901@users.noreply.github.com>
2021-05-29 15:51:28 +0530

    Update SignedInLinks.js

--- React-redux-firebase-Createproject/src/components/layout/SignedInLinks.js
+++ React-redux-firebase-Createproject/src/components/layout/SignedInLinks.js
@@ -5,14 +5,11 @@ import signOut from '../../store/actions/authsignOut';
 
 const SignedInLinks=(props)=>{
   return(
-    <div>
-    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons"></i></a>
-    <ul id="nav-mobile" class="right hide-on-med-and-down">
+    <ul  class="right ">
       <li><NavLink to="/create">Create Project</NavLink></li>
       <li><button onClick={props.signOut}  className="white-text btn-flat grey darken-3 ">Log out</button></li>
       <li><NavLink to="/" className="btn btn-floating ">{props.profile.initial}</NavLink></li>
     </ul>
-    </div>
   )
 }
 
